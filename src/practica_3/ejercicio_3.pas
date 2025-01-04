bce0e2965ebd748a7f2e5f109403f393523b9127
brunozenobio <brunozenobio4@gmail.com>
2025-01-04 02:42:33 -0300

    Ejercicio 3 finalziado

--- src/practica_3/ejercicio_3.pas
+++ src/practica_3/ejercicio_3.pas
@@ -2,8 +2,7 @@ program  alcanceYFunciones;
 var
     suma,cant:integer;
 function calcularPromedio:real;
-var
-    prom:real;
+
 begin
     if (cant=0) then
         calcularPromedio:=-1
@@ -13,3 +12,11 @@ end;
 begin {programa principal}
     readln(suma);
     readln(cant);
+    promedio :=calcularPromedio
+    if (promedio<>-1) then begin
+        writeln('El promedio es:',promedio:2:1)
+        end
+    else
+        writeln('No se puede dividir por 0');
+
+end.
