8757fbb95d1e5309329c618451c6dd9a1b6dcdd5
brunozenobio <brunozenobio4@gmail.com>
2025-01-05 01:09:21 -0300

    Ejercicio 4 de practica 3

---
+++ src/practica_3/ejercicio_4.pas
@@ -0,0 +1,25 @@
+{
+Autor : Bruno Zenobio
+Version : 1.0
+Description : Practica 1, Ejercicio 1a}
+
+program Ejercicio1;
+//Este programa lee dos numeros por teclados y calcula el promedio
+function suma(a,b:double):double;
+
+begin
+
+    suma:=a+b;
+
+end;
+
+var
+        num1,num2:double;
+begin;
+        writeln('Ingrese el numero 1: ');
+        readln(num1);
+        writeln('Ingrese el segundo numero');
+        readln(num2);
+        writeln('Suma: ',suma(num1,num2):6:1);
+
+end.
