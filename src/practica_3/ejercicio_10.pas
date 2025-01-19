1a613820d0b52808ae2bcfc505972607181c9bd5
brunozenobio <brunozenobio4@gmail.com>
2025-01-19 16:11:39 -0300

    Ejercicio 10

--- src/practica_3/ejercicio_10.pas
+++ src/practica_3/ejercicio_10.pas
@@ -7,19 +7,18 @@ program Ejercicio1;
 //Este programa lee dos numeros por teclados y calcula el promedio
 
 
-function factorial(n:integer);
-var fact:integer;
+function factorial(n:integer):integer;
 begin
-        if (n = 0 | n = 1) then
-			factorial = fact
-		else then
-			fact = factorial(n-1) * fact
+        if ((n = 0 )or (n = 1)) then
+			factorial := 1
+		else 
+			factorial := factorial(n-1) * n;
 end;
 var
     n:integer;
 begin
        write('Calculando factorial ingrese el deseado: ');
-       readln(n)
-       writeln('El factorial de', n, 'es: ',factorial(n))
+       readln(n);
+       writeln('El factorial de ', n, 'es: ',factorial(n));
 
 end.
